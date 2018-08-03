98ba7ea5e741ea87fa33076faa44833fc3e63e2e
Garrett Adams <cyrodillsavior@gmail.com>
2018-08-03 00:51:07 -0400

    Remoed opening with scanner, implimented scan button functionality.

--- app/src/main/java/advantage_demo/m10cpu/com/advantage_demo/MainActivity.java
+++ app/src/main/java/advantage_demo/m10cpu/com/advantage_demo/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        openScanner();
+        //openScanner();
     }
 
     @Override
@@ -41,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
                 openScanner();
             }
 
+    }
+
+    public void scanButtonClicked(View view){
+        openScanner();
     }
 
     private void openScanner(){
